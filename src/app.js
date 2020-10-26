d88b65a74f2212a65a2da8b8f3910fb81828e1e7
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-10-26 20:57:19 +0200

    fix import for wakeUpDyno

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bot = require('./bot/bot');
 const connectDb = require('./db/db-connect');
-const { wakeUpDyno } = require('./helpers/herokuAntiIdle');
+const wakeUpDyno = require('./helpers/herokuAntiIdle');
 
 connectDb().then(() => console.log('connect to db success'));
 bot.launch().then(() => console.log('bot up and running'));
