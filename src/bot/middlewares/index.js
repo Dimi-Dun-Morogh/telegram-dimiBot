93d82162bee7c1f485a5433a6d1733dcdce6ba4c
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-10-31 20:29:29 +0200

    another fix to isInGroupMiddleWare

--- src/bot/middlewares/index.js
+++ src/bot/middlewares/index.js
@@ -1,5 +1,5 @@
 const isInGroupMiddleWare = () => (ctx, next) => {
-  if (ctx === undefined || ctx === null || ctx.message === null) return null;
+  if (ctx === undefined || ctx === null || ctx.message === undefined) return null;
   const { chat, text } = ctx.message;
   const allowedCommands = ['/help', '/joke', '/start'];
   if (chat.type === 'private' && !allowedCommands.includes(text))
