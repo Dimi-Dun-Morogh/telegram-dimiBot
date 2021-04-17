018a5541c8fe7584f9521bb72bf6235274e562f6
Dimi-Dun-Morogh <darnelbro@gmail.com>
2021-04-17 20:07:39 +0300

    quick fix to getWordStats :100, word under 3 chars crashed the script

--- src/bot/handlers/messages/index.ts
+++ src/bot/handlers/messages/index.ts
@@ -93,9 +93,11 @@ const getMyStats = async (context: TelegrafContext) => {
 };
 
 const getWordStats = async (context: TelegrafContext) => {
-  const { chat, text } = context.message!;
+  const { chat, text, message_id } = context.message!;
   const [, word] = text!.split(' ');
+
   if (word === undefined) return context.reply('укажите слово через пробел после /stat_word ');
+  if (word.length < 3) return context.reply('не балуй', { reply_to_message_id: message_id });
   const messages = await getChatMessages(chat.id);
   if (!messages) return null;
   const stats = renderMsg.wordStatsStr(messages, word);
