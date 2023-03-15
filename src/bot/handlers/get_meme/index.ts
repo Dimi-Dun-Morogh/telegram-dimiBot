a1a8c6dbee62fcf74d1c52841ebeeba32a3e859c
Dimi-Dun-Morogh <darnelbro@gmail.com>
2023-03-15 20:02:01 +0300

    got rid of puppeteer due to hosting limits

--- src/bot/handlers/get_meme/index.ts
+++ src/bot/handlers/get_meme/index.ts
@@ -1,7 +1,7 @@
 import { TelegrafContext } from 'telegraf/typings/context';
 import logger from '../../../helpers/loggers';
 
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
 const NAMESPACE = 'get_meme/index.ts';
 
@@ -9,37 +9,37 @@ const memeBank = {
   url: 'https://admem.ru/rndm',
   memes: [],
   async parseMemes() {
-    try {
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--no-first-run',
-          '--no-zygote',
-          '--single-process', // <- this one doesn't works in Windows
-          '--disable-gpu',
-        ],
-      });
-      const page = await browser.newPage();
-      await page.goto(this.url, { waitUntil: 'networkidle2' });
+    // try {
+    //   const browser = await puppeteer.launch({
+    //     headless: true,
+    //     args: [
+    //       '--no-sandbox',
+    //       '--disable-setuid-sandbox',
+    //       '--disable-dev-shm-usage',
+    //       '--disable-accelerated-2d-canvas',
+    //       '--no-first-run',
+    //       '--no-zygote',
+    //       '--single-process', // <- this one doesn't works in Windows
+    //       '--disable-gpu',
+    //     ],
+    //   });
+    //   const page = await browser.newPage();
+    //   await page.goto(this.url, { waitUntil: 'networkidle2' });
 
-      const parsedMems = await page.evaluate(() => {
-        const allTexts = document.querySelectorAll('.post-img');
-        if (!allTexts.length) return;
-        // @ts-ignore
-        return [...allTexts].map((elem) => `https:${elem.firstElementChild!.firstElementChild!.getAttribute('src')}`);
-      });
+    //   const parsedMems = await page.evaluate(() => {
+    //     const allTexts = document.querySelectorAll('.post-img');
+    //     if (!allTexts.length) return;
+    //     // @ts-ignore
+    //     return [...allTexts].map((elem) => `https:${elem.firstElementChild!.firstElementChild!.getAttribute('src')}`);
+    //   });
 
-      logger.info(NAMESPACE, 'parsed memes', parsedMems);
-      this.memes = parsedMems;
+    //   logger.info(NAMESPACE, 'parsed memes', parsedMems);
+    //   this.memes = parsedMems;
 
-      browser.close();
-    } catch (error) {
-      logger.error(NAMESPACE, 'err in parseMemes', error);
-    }
+    //   browser.close();
+    // } catch (error) {
+    //   logger.error(NAMESPACE, 'err in parseMemes', error);
+    // }
   },
   async getMeme(): Promise<string> {
     if (!this.memes.length) { await this.parseMemes(); }
@@ -49,6 +49,7 @@ const memeBank = {
 
 const handleMeme = async (ctx: TelegrafContext) => {
   try {
+    return;
     const imageUrl = await memeBank.getMeme();
     ctx.replyWithPhoto(imageUrl);
   } catch (error) {
